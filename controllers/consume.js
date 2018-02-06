e6d2a2f3eafd40e4039f30ff4755723ace1ad5f7
franciscojosetis <franciscojosetis@gmail.com>
2018-02-06 15:59:26 +0100

    Arreglo fechas de consumos

--- controllers/consume.js
+++ controllers/consume.js
@@ -48,3 +48,73 @@ module.exports.consumo = (req, res) => {
     });
 
 };
+
+module.exports.consumoHoy = (req, res) => {
+
+    Placa.findOne({id_placa: req.body.id_placa}, (err, placa) => {
+        if (err) return res.status(404).jsonp({error: 404, mensaje: 'No existe una Placa con ese UUID'});
+
+        /*let ahora = new Date();
+        console.log("Now " + ahora);
+        console.log(ahora.getYear() + " : " + ahora.getMonth() + " : " + ahora.getDate());
+        let start_date = new Date(ahora.getYear(), ahora.getMonth(), ahora.getDate(), 0, 0, 0, 0);
+        console.log(start_date);
+        let end_date = new Date();
+        end_date.setDate(start_date.getTime () + 1 * 86400000);*/
+
+        let start_date = Moment().startOf('day');
+        let end_date = Moment().add(1, 'd');
+        //let start_date = ahora.
+
+        Consumo
+            .find({id_placa: placa})
+            .select('consumo fecha_Inicio fecha_Fin -_id')
+            .where('fecha_Inicio').gte(start_date)
+            .where('fecha_Inicio').lt(end_date)
+            .exec((err, consumo) => {
+            if (err) return res.status(500).jsonp({
+                error: 500,
+                mensaje: `${err.message}`
+            });
+            res.status(200).jsonp(consumo);
+        });
+    });
+
+};
+
+module.exports.consumoDia = (req, res) => {
+
+    if (!req.params.fecha) {
+        return res.sendStatus(400);
+    }
+
+    Placa.findOne({id_placa: req.body.id_placa}, (err, placa) => {
+        if (err) return res.status(404).jsonp({error: 404, mensaje: 'No existe una Placa con ese UUID'});
+
+        /*let ahora = new Date();
+        console.log("Now " + ahora);
+        console.log(ahora.getYear() + " : " + ahora.getMonth() + " : " + ahora.getDate());
+        let start_date = new Date(ahora.getYear(), ahora.getMonth(), ahora.getDate(), 0, 0, 0, 0);
+        console.log(start_date);
+        let end_date = new Date();
+        end_date.setDate(start_date.getTime () + 1 * 86400000);*/
+
+        let start_date = Moment(req.params.fecha).startOf('day');
+        let end_date = Moment(req.params.fecha).add(1, 'd');
+        //let start_date = ahora.
+
+        Consumo
+            .find({id_placa: placa})
+            .select('consumo fecha_Inicio fecha_Fin -_id')
+            .where('fecha_Inicio').gte(start_date)
+            .where('fecha_Inicio').lt(end_date)
+            .exec((err, consumo) => {
+                if (err) return res.status(500).jsonp({
+                    error: 500,
+                    mensaje: `${err.message}`
+                });
+                res.status(200).jsonp(consumo);
+            });
+    });
+
+};
--- routes/consume.js
+++ routes/consume.js
@@ -5,5 +5,7 @@ const auth = require('../middlewares/auth');
 
 router.post('/', consumeController.nuevoDato);
 router.post('/consumo', auth.isAuth, consumeController.consumo);
+router.post('/consumo/hoy', auth.isAuth, consumeController.consumoHoy);
+router.post('/consumo/dia/:fecha', auth.isAuth, consumeController.consumoDia);
 
 module.exports = router;
